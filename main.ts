ccba71076722e85fa6a56b9038d1657aab8813a5
gavin-gallant <gavin.gallant@mths.ca>
2024-09-10 10:32:50 -0400

    updated comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Gavin Gallant
+ * Created on: Sep 2024
+ * This program says hello world
 */
 
 basic.showString('Hello, World!')
